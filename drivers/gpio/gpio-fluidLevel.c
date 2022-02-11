8e27e65b994cd007cb2eadc1bb1bfb6f3b974974
chentuo <chentuo@soonsolid.com>
2022-02-11 14:29:39 +0800

    add char device for fluid level

--- drivers/gpio/gpio-fluidLevel.c
+++ drivers/gpio/gpio-fluidLevel.c
@@ -20,6 +20,12 @@ struct fluid_level_data {
 static struct fluid_level_data *fluid_level_datas;
 static struct gpio_descs *fluid_level_descs;
 
+#define FLUID_LEVEL_MAJOR_NAME "fluid_level_major"
+#define FLUID_LEVEL_CLASS_NAME "fluid_level_class"
+#define FLUID_LEVEL_DEVICE_NAME "fluid_levels"
+static int fluid_level_major = 0;
+static struct class *fluid_level_class;
+
 static irqreturn_t fluid_level_irq_request(int irq, void *dev_id)
 {
 	struct fluid_level_data *fluid_level = dev_id;
@@ -30,10 +36,78 @@ static irqreturn_t fluid_level_irq_request(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
+static ssize_t fluid_level_read(struct file *file, char __user *buf, size_t size, loff_t *offset)
+{
+	
+	return 0;
+}
+
+static struct file_operations fluid_level_ops = {
+	.owner = THIS_MODULE,
+	.read = fluid_level_read,
+};
+
+static int create_fluid_level_chrdev(void)
+{
+	fluid_level_major = register_chrdev(0, FLUID_LEVEL_MAJOR_NAME,
+			&fluid_level_ops);
+	if(fluid_level_major < 0) {
+		printk(KERN_ERR"fluid level: couldn't get a major number\n");
+		return -1;
+	}
+
+	fluid_level_class = class_create(THIS_MODULE, FLUID_LEVEL_CLASS_NAME);
+	if(IS_ERR(fluid_level_class)) {
+		printk(KERN_ERR"fluid level class:create failed\n");
+		unregister_chrdev(fluid_level_major, FLUID_LEVEL_MAJOR_NAME); 
+		return -1;
+	}
+
+	device_create(fluid_level_class, NULL, MKDEV(fluid_level_major, 0),
+			NULL, FLUID_LEVEL_DEVICE_NAME); 
+	return 0;
+}
+
+static void delete_fluid_level_chrdev(void)
+{
+	if(!IS_ERR(fluid_level_class)) {
+		device_destroy(fluid_level_class, MKDEV(fluid_level_major, 0));
+		class_destroy(fluid_level_class);
+	}
+
+	if(fluid_level_major>=0) {
+		unregister_chrdev(fluid_level_major, FLUID_LEVEL_MAJOR_NAME);
+	}
+}
+
+static int request_fluid_level_irq(struct platform_device *pdev, struct device_node *node, struct fluid_level_data *data)
+{
+	int err = 0;
+	if(!node) return -1;
+
+	data->irq = of_irq_get(node, data->index);
+	if(data->irq <= 0) {
+		dev_err(&pdev->dev, 
+				"Failed to get fluid level %d irq error:%d\n", 
+				data->index, data->irq);
+		return -1;
+	}
+	err = request_irq(data->irq, fluid_level_irq_request,
+			IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING,
+			"fluid-level-irq", data);
+	if(err) {
+		dev_err(&pdev->dev, 
+				"Failed to reuqest fluid level %d irq for driver\n", data->index);
+	}
+	printk(KERN_WARNING"fluid level %d driver get irq num:%d \n", 
+			data->index, data->irq);
+	return 0;
+}
+
 static int fluid_level_probe(struct platform_device *pdev)
 {
 	struct device_node *node = pdev->dev.of_node;
-	int i = 0, err, count = 0;
+	int i = 0, count = 0;
 	fluid_level_descs = devm_gpiod_get_array(&pdev->dev, 
 			"fluidLevel", GPIOD_IN);
 	if(IS_ERR(fluid_level_descs)) {
@@ -51,29 +125,14 @@ static int fluid_level_probe(struct platform_device *pdev)
 		return PTR_ERR(fluid_level_datas);
 	}
 	
-	
 	for(i=0;i<fluid_level_descs->ndescs;i++) {
 		fluid_level_datas[i].desc = fluid_level_descs->desc[i];
 		fluid_level_datas[i].index = i;
-		if(node) {
-			fluid_level_datas[i].irq = of_irq_get(node, i);
-			if(fluid_level_datas[i].irq <= 0) {
-				dev_err(&pdev->dev, 
-						"Failed to get fluid level %d irq error:%d\n", 
-						i, fluid_level_datas[i].irq);
-				break;
-			}
-			err = request_irq(fluid_level_datas[i].irq, fluid_level_irq_request,
-					IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING,
-					"fluid-level-irq", &fluid_level_datas[i]);
-			if(err) {
-				dev_err(&pdev->dev, 
-						"Failed to reuqest fluid level %d irq for driver\n", i);
-			}
-			printk(KERN_WARNING"fluid level %d driver get irq num:%d \n", 
-					i, fluid_level_datas[i].irq);
-		}
+		if(request_fluid_level_irq(pdev, node, &fluid_level_datas[i]) < 0)
+			break;
 	}
+
+	create_fluid_level_chrdev();
 
 	return 0;
 }
@@ -81,6 +140,8 @@ static int fluid_level_probe(struct platform_device *pdev)
 static int fluid_level_remove(struct platform_device *pdev)
 {
 	int i = 0;
+
+	delete_fluid_level_chrdev();
 	
 	for(i=0;i<fluid_level_descs->ndescs;i++) {
 		free_irq(fluid_level_datas[i].irq, &fluid_level_datas[i]);
